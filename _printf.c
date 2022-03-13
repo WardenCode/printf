6e1dc3f3a7653ffba6276d18d671c13f21beb1d6
Diego <diegojeanluck@hotmail.com>
2022-03-13 10:14:19 -0500

    feat print the string Co-authored-by: Luis Fernando Manrique Chávez

--- _printf.c
+++ _printf.c
@@ -10,5 +10,28 @@
 
 int _printf(const char *format, ...)
 {
-	
+	int i = 0, j = 0, final_size = 0;
+	char *string_formated = NULL;
+
+	/*Count the size of the format*/
+	for (i = 0; format[i] != '\0'; i++)
+		continue;
+
+	/*create the space to the formated string*/
+	string_formated = malloc(i * sizeof(char));
+	final_size = i;
+
+	while(j < i)
+	{
+		string_formated[j] = format[j];
+		j++;
+	}
+
+	string_formated[i] = '\0';
+
+	write(1, string_formated, final_size);
+
+	/*print_all(string_formated);*/
+	free(string_formated);
+	return (final_size);
 }

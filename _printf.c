a661885d3d6a1a4c8779a2e2508ec7fb5dd39ab4
Diego <diegojeanluck@hotmail.com>
2022-03-13 09:21:19 -0500

    created the header file, and principal functions

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _printf - Produce output according to a format as described below.
+ *
+ * @format: A string with the format wished.
+ *
+ * Return: The length of the final string with the format
+ */
+
+int _printf(const char *format, ...)
+{
+	
+}

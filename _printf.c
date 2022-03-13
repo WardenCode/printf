c90c538ed153bac2c8a6853b150eb24d35cf7679
Luis Manrique <luismanrique158158@gmail.com>
2022-03-13 11:07:06 -0500

    Feat print_all function

--- _printf.c
+++ _printf.c
@@ -24,6 +24,24 @@ int _strlen(char *string)
 }
 
 /**
+ * print_all - Print into the standard output an string
+ *
+ * @string: pointer to the first letter of a string
+ *
+ * @size: Size of the string (count the null space)
+ *
+ * Return: Void
+ */
+
+void print_all(char *string, unsigned int size)
+{
+	if (string && size >= 0)
+		write(1, string, (size - 1));
+
+	write(1, "You must to enter an string (NOT NULL)", 39);
+}
+
+/**
  * _printf - Produce output according to a format as described below.
  *
  * @format: A string with the format wished.

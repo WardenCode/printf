611d06f74da174ca16bec8943fe4c06c24c083fd
Diego <diegojeanluck@hotmail.com>
2022-03-13 11:36:55 -0500

    fix betty style/doc and the names of arguments and its types

--- _printf.c
+++ _printf.c
@@ -8,15 +8,16 @@
  * Return: The length of an string
  */
 
-int _strlen(char *string)
+int _strlen(const char *string)
 {
 	int size = 0;
+
 	if (string)
 	{
 		while (string[size] != '\0')
-		size++;
+			size++;
 
-		return (size);
+		return (size + 1);
 	}
 
 	write(1, "You must to enter an string (NOT NULL)", 39);
@@ -33,34 +34,36 @@ int _strlen(char *string)
  * Return: Void
  */
 
-void print_all(char *string, unsigned int size)
+void print_all(void *string, int size)
 {
 	if (string && size >= 0)
+	{
 		write(1, string, (size - 1));
+		return;
+	}
 
 	write(1, "You must to enter an string (NOT NULL)", 39);
 }
 
-
 /**
- * fill_malloc - Fill a string previously allocated.
+ * fill_alloc - Fill a string previously allocated.
  *
- * @allocated_string: Pointer to the spaces allocated.
+ * @alloc_str: Pointer to the spaces allocated.
  *
- * @string_base: A string with the format wished.
+ * @str_base: A string with the format wished.
  *
- * @size_to_fill: quantity to fill the allocated string.
+ * @size_fill: quantity to fill the allocated string.
  *
  * Return: Void
  */
 
-void fill_malloc(char *allocated_string, char *string_base, unsigned int size_to_fill)
+void fill_alloc(char *alloc_str, const char *str_base, int size_fill)
 {
 	int i = 0;
 
-	while(i < size_to_fill)
+	while (i < size_fill)
 	{
-		allocated_string[i] = string_base[i];
+		alloc_str[i] = str_base[i];
 		i++;
 	}
 }
@@ -75,14 +78,14 @@ void fill_malloc(char *allocated_string, char *string_base, unsigned int size_to
 
 int _printf(const char *format, ...)
 {
-	int size_initial = 0, j = 0, final_size = 0;
+	int size_initial = 0, final_size = 0;
 	char *string_formated = NULL;
 
 	size_initial = _strlen(format);
 
-	string_formated = malloc(i * sizeof(char));
+	string_formated = malloc(size_initial * sizeof(char));
 	final_size = size_initial;
-    fill_malloc(string_formated, format, size_initial);
+	fill_alloc(string_formated, format, size_initial);
 
 	print_all(string_formated, size_initial);
 	free(string_formated);

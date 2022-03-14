0056e568d25d63ff4c1b07431de8befc755d4adf
Diego <diegojeanluck@hotmail.com>
2022-03-13 22:03:20 -0500

    feat now the functions (print_str, char, percentage) and works

---
+++ char_options.c
@@ -0,0 +1,112 @@
+#include "main.h"
+
+/**
+ * print_num - Print a num;
+ *
+ * @arg: Num to print
+ *
+ * Return: The length of the num.
+ */
+
+int print_num(void *arg)
+{
+	int *argument = arg;
+	int number = argument[0];
+	int holder = 0;
+	int count = 0;
+
+	if (number < 0)
+	{
+		count++;
+		number *= -1;
+	}
+
+	holder = number;
+
+	while (number != 0)
+	{
+		number /= 10;
+		count++;
+	}
+	write(1, &holder, count);
+	return(1);
+}
+
+/**
+ * op_sub - ubstract b to a
+ *
+ * @arg: Char to print
+ *
+ * Return: 1.
+ */
+
+int print_char(void *arg)
+{
+	write(1, &arg, 1);
+	return (1);
+}
+
+/**
+ * ptrint_str - Print a string;
+ *
+ * @arg: The string to print
+ *
+ * Return: The length of an string
+ */
+
+int print_str(void *arg)
+{
+	char *argument = arg;
+	int i = 0;
+
+	while (argument[i])
+	{
+		_putchar(argument[i]);
+		i++;
+	}
+	return (i - 1);
+}
+
+/**
+ * print_int - Print a int
+ *
+ * @arg: The int to print
+ *
+ * Return: The length of int
+ */
+
+int print_int(void *arg)
+{
+	int *argument = arg;
+	int number = argument[0];
+	int holder = 0;
+	int count = 0;
+
+	if (number < 0)
+	{
+		count++;
+		number *= -1;
+	}
+
+	holder = number;
+
+	while (number != 0)
+	{
+		number /= 10;
+		count++;
+	}
+	write(1, &holder, count);
+	return(1);
+}
+
+/**
+ * print_percent- Print a percentaje symbol
+ *
+ * Return: 1;
+ */
+
+int print_percent(void *arg __attribute__((unused)))
+{
+	_putchar('%');
+	return (0);
+}

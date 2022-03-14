44b4e216c36c400255d31a3f813b99fc78fefd1d
Luis Manrique <luismanrique158158@gmail.com>
2022-03-13 21:54:25 -0500

    Adding struct print_operation, all operations, _strcmp and fix special chars

--- main.h
+++ main.h
@@ -8,14 +8,25 @@
 
 typedef struct special_chars
 {
-    char *op;
-    void (*operation)(char *string, void *argument);
+    int op;
+    int (*operation)(void *arg);
 } special_chars_t;
+
+typedef struct print_operation
+{
+    int flag;
+    int (*print)(void *arg);
+} print_operation_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 int _strlen(const char *string);
-void print_all(void *string, int size);
-void fill_alloc(char *alloc_str, const char *str_base, int size_fill);
+int _strcmp(char *s1, char *s2);
+print_operation_t choose_option(char s);
+int print_num(void *arg);
+int print_char(void *arg);
+int print_str(void *arg);
+int print_int(void *arg);
+int print_percent(void *arg);
 
 #endif

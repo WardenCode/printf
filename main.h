ee4bbcfcc184ccebfb5d1ace011b04eea4b0425c
Luis Manrique <luismanrique158158@gmail.com>
2022-03-15 12:33:25 -0500

    Adding prototypes of functions print_num_binary, print_str_rev, print_rot13 and rot13

--- main.h
+++ main.h
@@ -22,7 +22,7 @@ typedef struct special_chars
 /**
  * struct print_operation - Contain and call the function to print
  *
- * @flag: A flag to validate if exitst a special char (%?).
+ * @flag: A flag to validate if exist a special char (%?).
  *
  * @print: function to print something.
  */
@@ -45,5 +45,12 @@ int print_char(va_list args);
 int print_str(va_list args);
 int print_int(va_list args);
 int print_percent(va_list args);
+
+int print_rot13(va_list args);
+char *rot13(char *str);
+int print_binary(va_list args);
+void print_num_binary(int n);
+void print_number(int n);
+int print_str_rev(va_list args);
 
 #endif
